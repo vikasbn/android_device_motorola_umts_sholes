318dea1626a6cb01898a8e809ad2305320001442
Micha LaQua <micha.laqua@gmail.com>
2011-07-04 00:13:35 +0200

    workaround: wlan binaries were not correctly added to the package

--- umts_sholes.mk
+++ umts_sholes.mk
@@ -95,6 +95,8 @@ PRODUCT_COPY_FILES += \
     device/motorola/umts_sholes/prebuilt/bin/2nd-init:/system/bin/2nd-init \
     device/motorola/umts_sholes/prebuilt/bin/init_prep_keypad.sh:/system/bin/init_prep_keypad.sh \
     device/motorola/umts_sholes/prebuilt/bin/sh_hijack.sh:/system/bin/sh_hijack.sh \
+    device/motorola/umts_sholes/prebuilt/bin/wlan_cu:/system/bin/wlan_cu \
+    device/motorola/umts_sholes/prebuilt/bin/wlan_loader:/system/bin/wlan_loader \
     device/motorola/umts_sholes/prebuilt/usr/keychars/sholesp2a-keypad.kcm.bin:/system/usr/keychars/sholesp2a-keypad.kcm.bin \
     device/motorola/umts_sholes/prebuilt/app/Usb.apk:/system/app/Usb.apk \
     device/motorola/umts_sholes/prebuilt/app/Toggle2G.apk:/system/app/Toggle2G.apk \
