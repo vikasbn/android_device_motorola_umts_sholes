319dda756a7289dcd28750bec2653092ba3142b4
Micha LaQua <micha.laqua@gmail.com>
2011-07-28 19:44:00 +0200

    multitouch: use new prop

--- prebuilt/bin/init_fivemt.sh
+++ prebuilt/bin/init_fivemt.sh
@@ -1,7 +1,7 @@
 #!/system/bin/sh
 export PATH=/system/bin:/system/xbin:$PATH
 
-mt=`getprop persist.sys.fivemt`
+mt=`getprop persist.sys.five_multitouch`
 if [ "$mt" = "1" ]; then
    insmod /system/lib/modules/qtouch_num.ko
 fi
